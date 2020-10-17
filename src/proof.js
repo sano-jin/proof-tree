b509f08ce75278ff2b9791a8017f6c6a3ef8c421
Jin SANO <sano-jin@akane.waseda.jp>
2020-10-17 22:30:55 +0900

    updated

--- src/proof.js
+++ src/proof.js
@@ -1,23 +1,33 @@
 "use strict";
-const tree = {'str': "hoge"
-	      , 'trans': "num"
-	      , "children": [
-		  {'str': "hoge"
-		   , 'trans': "num"
-		   , "children": []}, 
-		  {'str': "hoge"
-		   , 'trans': "num"
-		   , "children": [
-		       {'str': "hoge"
-			, 'trans': "num"
-			, "children": []},
-		       {'str': "hoge"
-			, 'trans': "num"
-			, "children": []}
+const tree = {'consequent': "hoge"
+	      , 'rule': "num"
+	      , "antecedents": [
+		  {'consequent': "hoge"
+		   , 'rule': "num"
+		   , "antecedents": []}, 
+		  {'consequent': "hoge"
+		   , 'rule': "num"
+		   , "antecedents": [
+		       {'consequent': "hoge"},
+		       {'consequent': "hoge"
+			, 'rule': "num"
+			, "antecedents": []}
 		   ]},
-		  {'str': "hoge"
-		   , 'trans': "num"
-		   , "children": []}
+		  {'consequent': "hoge"
+		   , 'rule': "num"
+		   , "antecedents": [
+		       {'consequent': "hoge"
+			, 'rule': "num"
+			, "antecedents": []}, 
+		       {'consequent': "hoge"
+			, 'rule': "num"
+			, "antecedents": [
+			    {'consequent': "hoge"},
+			    {'consequent': "hoge"
+			     , 'rule': "num"
+			     , "antecedents": []}
+			]}
+		   ]}
 	      ]};
 
 const h = 1;
@@ -25,28 +35,39 @@ console.log("hello");
 console.log(tree);
 
 
-const createDiv = () => document.createElement("div"); 
-
-const node = document.createElement("span"); 
-console.log( node );
-const strTextNode = document.createTextNode("hgehogehoge"); 
-node.appendChild( strTextNode );
-console.log( node.scrollWidth );
-
-
-/*
-function getNodeAndWidth ( str, trans, children ) { 
-    const nodeDiv = createDiv;
-    const strDiv = createDiv;
-    const transDiv = createDiv;
-    // const childrenDiv = createDiv;
-    const strTextNode = document.createTextNode( str ); 
-    const transTextNode = document.createTextNode( trans ); 
-    
-    
-    nodeDiv.appendChild(newContent);
-    return [ newDiv, width ];
-  const currentDiv = document.getElementById("div1"); 
-  document.body.insertBefore(newDiv, currentDiv); 
+const createDiv = ( className ) => {
+    const div = document.createElement("div");
+    if ( className ) div.classList.add( className );
+    return div;
 }
-*/
+const createTextDiv = ( str, className ) => {
+    const div = createDiv( className );
+    div.appendChild( document.createTextNode( str ) );
+    return div;
+}
+    
+function getNode ( json ) { 
+    const nodeDiv = createDiv( "proof-tree-node" );
+    const consequentDiv = createTextDiv( json.consequent, "proof-tree-consequent" );
+    if ( json.rule ) {
+	const antecedentsDiv = createDiv( "proof-tree-antecedents" );
+	json.antecedents
+	    .map(a => getNode( a ))
+	    .map(aDiv => antecedentsDiv.appendChild( aDiv ) );
+	nodeDiv.appendChild( antecedentsDiv );
+	nodeDiv.appendChild( createDiv( "proof-tree-bar" ) );
+	nodeDiv.appendChild( createTextDiv ( json.rule, "proof-tree-rule" ) );
+    }
+    nodeDiv.appendChild( consequentDiv );
+    return nodeDiv; 
+}
+
+window.onload = function(){
+    const rootDiv = document.getElementById( "proof-tree" );
+    console.log( rootDiv );
+    rootDiv.removeChild(rootDiv.childNodes[0]);  
+    rootDiv.appendChild( getNode( tree ) );
+    console.log( rootDiv.offsetHeight );
+}
+
+			       
